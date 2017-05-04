a63f123cc1a27fd13001511f55006afa969831d0
Przemysław Poljański <Peperzastey@users.noreply.github.com>
2017-05-05 01:52:52 +0200

    Fixing build.sh: Remove gradlew connectedCheck

--- travis-scripts/build.sh
+++ travis-scripts/build.sh
@@ -2,7 +2,8 @@
 set -ev
 
 if [ "${BUILD}" = "android" ]; then
-	bash ./ANDROID/ForecasterApp/gradlew build connectedCheck
+	bash ./ANDROID/ForecasterApp/gradlew build 
+	# TODO? connectedCheck
 elif [ "${BUILD}" = "avr" ]; then
 	platformio ci --board=uno
 fi
